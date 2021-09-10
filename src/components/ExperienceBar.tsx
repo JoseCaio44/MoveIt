fb0ee3707a0d66133b8e9b44fb35d7725c952728
JoseCaio44 <jose.caio@academico.ifg.edu.br>
2021-09-09 23:32:28 -0300

    experience bar

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,9 @@
+export function ExperienceBar() {
+    return (
+        <header className="experience-bar">
+            <span>0 XP</span>
+            <div />
+            <span>0 XP</span>
+        </header>
+    );
+}
