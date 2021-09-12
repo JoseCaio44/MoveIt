147e169ee625e7021ba8d8f108e3a78d47542ef0
JoseCaio44 <jose.caio@academico.ifg.edu.br>
2021-09-11 23:31:50 -0300

    experience bar refactor

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -2,7 +2,11 @@ export function ExperienceBar() {
     return (
         <header className="experience-bar">
             <span>0 XP</span>
-            <div />
+            <div>
+                <div style={{ width: '60%' }} />
+
+                <span className="current-experience" style={{ left: '50%' }}> 300 XP </span>
+            </div>
             <span>0 XP</span>
         </header>
     );
